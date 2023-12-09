2d3c0d00590a428c80a55ec6227e7ac32d7bf88c
Lbitwd <114842775+Lbitwd@users.noreply.github.com>
2023-12-09 18:18:42 -0500

    Add files via upload

---
+++ JobPulse/JobPulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobPulse
+//
+//  Created by Shuya Yang on 12/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
